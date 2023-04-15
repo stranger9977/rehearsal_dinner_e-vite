2b20a38d1dbcf186a0633a42ea5ddeb586010163
stranger9977 <nickgurol@gmail.com>
2023-04-15 18:58:19 -0400

    adding files for heroku

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ def update_guest_data(guests_by_pair):
 csv_filename = 'guests.csv'
 guests_by_pair = generate_guest_data(csv_filename)
 
-@app.route('/', defaults={'pair_id': 5980}, methods=['GET', 'POST'])
+@app.route('/', defaults={'pair_id': None}, methods=['GET', 'POST'])
 @app.route('/<pair_id>', methods=['GET', 'POST'])
 def index(pair_id=None):
     selected_pair_id = request.args.get('pair_id', "5980")  # default pair ID
