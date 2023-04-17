a518e5c2290d565a75cfe5b38622c586bcf2185b
stranger9977 <nickgurol@gmail.com>
2023-04-16 23:43:41 -0400

    fixing issue with urls

--- app.py
+++ app.py
@@ -87,14 +87,22 @@ def update_guest_rsvp(pair_id, guest_name, rsvp_status, guests_by_pair):
         guests_by_pair[pair_id][guest_name]["rsvpStatus"] = rsvp_status
 
 def update_guest_data(guests_by_pair):
-    with open('guests.csv', mode='r') as csvfile:
-        reader = csv.DictReader(csvfile)
-        updated_rows = []
+    with open('guest_data.csv', mode='r') as infile:
+        reader = csv.DictReader(infile)
+        data = [row for row in reader]
 
-        for row in reader:
-            if row['pair_id'] in guests_by_pair:
-                row.update(guests_by_pair[row['pair_id']])
-            updated_rows.append(row)
+    with open('guest_data.csv', mode='w', newline='') as outfile:
+        fieldnames = ['pair_id', 'name', 'rsvpStatus', 'appetizer', 'entree', 'dessert']
+        writer = csv.DictWriter(outfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+        for row_data in data:
+            pair_id = row_data['pair_id']
+            if pair_id in guests_by_pair:
+                row_data.update(guests_by_pair[pair_id])
+
+            writer.writerow(row_data)
+
 
     with open('guests.csv', mode='w', newline='') as csvfile:
         fieldnames = ['pair_id', 'name', 'rsvpStatus', 'appetizer', 'entree', 'dessert']
